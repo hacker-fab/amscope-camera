6171329db9910210d630ac6b0f61d238c6eaa885
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2023-03-18 01:19:15 -0400

    Update live_camera.py

--- live_camera.py
+++ live_camera.py
@@ -1,17 +1,15 @@
 # Basic camera test
 
 import cv2 as cv
-import matplotlib.pyplot as plt
 
 cam = cv.VideoCapture(0)
-
 result, image = cam.read()
 
 if result:
-    plt.imshow("AMSCOPE", image) # show image
-    plt.imwrite("AMSCOPE.png", image) # save image
+    cv.imshow("AMSCOPE", image) # show image
+    cv.imwrite("AMSCOPE.png", image) # save image
     
-    waitKey(0) # press key to close window
-    plt.destroywWindow("AMSCOPE")
+    cv.waitKey(0) # press key to close window
+    cv.destroywWindow("AMSCOPE")
 else:
     print("Image capture was unsuccessful")
