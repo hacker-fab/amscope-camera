2da860b890ee8f45bae399c18f0a0d1efd401606
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2023-03-18 01:10:16 -0400

    Update live_camera.py

--- live_camera.py
+++ live_camera.py
@@ -1,37 +1,17 @@
-# program to capture single image from webcam in python
+# Basic camera test
 
-# importing OpenCV library
 import cv2 as cv
+import matplotlib.pyplot as plt
 
-def take_photo():
-    # initialize the camera
-    # If you have multiple camera connected with
-    # current device, assign a value in cam_port
-    # variable according to that
-    cam_port = 0
-    cam = cv.VideoCapture(cam_port)
+cam = cv.VideoCapture(0)
 
-    # reading the input using the camera
-    result, image = cam.read()
+result, image = cam.read()
 
-    # If image will detected without any error,
-    # show result
-    if result:
-
-        # showing result, it take frame name and image
-        # output
-        imshow("AMSCOPE", image)
-
-        # saving image in local storage
-        imwrite("AMSCOPE.png", image)
-
-        # If keyboard interrupt occurs, destroy image
-        # window
-        waitKey(0)
-        destroyWindow("AMSCOPE")
-
-    # If captured image is corrupted, moving to else part
-    else:
-        print("No image detected. Please! try again")
-
-take_photo()
+if result:
+    plt.imshow("AMSCOPE", image) # show image
+    plt.imwrite("AMSCOPE.png", image) # save image
+    
+    waitKey(0) # press key to close window
+    plt.destroywWindow("AMSCOPE")
+else:
+    print("Image capture was unsuccessful")
