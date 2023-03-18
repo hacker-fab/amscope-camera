3ff821e7357d07ce687b33e54d6091840e888612
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2023-03-18 01:26:42 -0400

    Update live_camera.py

--- live_camera.py
+++ live_camera.py
@@ -1,15 +1,15 @@
 # Basic camera test
 
-import cv2 as cv
+import cv2
 
-cam = cv.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 result, image = cam.read()
 
 if result:
-    cv.imshow("AMSCOPE", image) # show image
-    cv.imwrite("AMSCOPE.png", image) # save image
+    cv2.imshow("AMSCOPE", image) # show image
+    cv2.imwrite("AMSCOPE.png", image) # save image
     
-    cv.waitKey(0) # press key to close window
-    cv.destroywWindow("AMSCOPE")
+    cv2.waitKey(0) # press key to close window
+    cv2.destroywWindow("AMSCOPE")
 else:
     print("Image capture was unsuccessful")
