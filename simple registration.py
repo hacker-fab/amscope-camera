5374a208c05bb89300798c37dc8b925d7b18fbfc
3lio <60524931+3lio@users.noreply.github.com>
2023-04-04 01:58:58 -0400

    Added registration to simple version
    Tested with bare image sensor. registration outputs near zero every time. Need to test with real image.

--- simple registration.py
+++ simple registration.py
@@ -2,6 +2,29 @@ import amcam
 import cv2
 import numpy as np
 
+def runRegistration(currImgArray, prevImgArray):
+    if prevImgArray is None:
+        return 0,0
+    # do registration stuff
+    # grayscale+float conversions
+    gray1 = np.float32(cv2.cvtColor(currImgArray, cv2.COLOR_BGR2GRAY))
+    gray2 = np.float32(cv2.cvtColor(prevImgArray, cv2.COLOR_BGR2GRAY))
+
+    # Calculate phase correlation
+    (dx, dy), _ = cv2.phaseCorrelate(gray1, gray2)
+
+    # Get image size
+    height, width = currImgArray.shape[:2]
+
+    # Calculate number of pixels mapped in X and Y directions
+    num_pixels_x = dx * width
+    num_pixels_y = dy * height
+
+    print("Number of pixels mapped in X direction:", num_pixels_x)
+    print("Number of pixels mapped in Y direction:", num_pixels_y)
+    # save this frame's array data for the next frame of registration
+    return num_pixels_x, num_pixels_y
+
 class App:
     def __init__(self):
         self.hcam = None
@@ -21,9 +44,12 @@ class App:
                 self.total += 1
                 print('pull image ok, total = {}'.format(self.total))
                 img = np.frombuffer(self.buf, dtype=np.uint8).reshape(self.height, self.width, 3)
-                if img.size > 0:
-                    cv2.imshow('image', img)
-                    cv2.waitKey(1)
+                cropwidth = 1000
+                img = img[self.height//2-cropwidth:self.height//2+cropwidth, self.width//2-cropwidth:self.width//2+cropwidth,]
+                cv2.imshow('image', img)
+                cv2.waitKey(1)
+                px,py = runRegistration(img, self.prevImg)
+                self.prevImg = img
             except amcam.HRESULTException as ex:
                 print('pull image failed, hr=0x{:x}'.format(ex.hr))
         else:
@@ -31,6 +57,7 @@ class App:
         
 
     def run(self):
+        self.prevImg = None
         a = amcam.Amcam.EnumV2()
         if len(a) > 0:
             print('{}: flag = {:#x}, preview = {}, still = {}'.format(a[0].displayname, a[0].model.flag, a[0].model.preview, a[0].model.still))
