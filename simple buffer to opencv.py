36a3d799a02e8d3456844e34cbb6d3e7879b53bc
3lio <60524931+3lio@users.noreply.github.com>
2023-04-04 01:28:21 -0400

    Create simple buffer to opencv.py

---
+++ simple buffer to opencv.py
@@ -0,0 +1,65 @@
+import amcam
+import cv2
+import numpy as np
+
+class App:
+    def __init__(self):
+        self.hcam = None
+        self.buf = None
+        self.total = 0
+
+# the vast majority of callbacks come from amcam.dll/so/dylib internal threads
+    @staticmethod
+    def cameraCallback(nEvent, ctx):
+        if nEvent == amcam.AMCAM_EVENT_IMAGE:
+            ctx.CameraCallback(nEvent)
+
+    def CameraCallback(self, nEvent):
+        if nEvent == amcam.AMCAM_EVENT_IMAGE:
+            try:
+                self.hcam.PullImageV2(self.buf, 24, None)
+                self.total += 1
+                print('pull image ok, total = {}'.format(self.total))
+                img = np.frombuffer(self.buf, dtype=np.uint8).reshape(self.height, self.width, 3)
+                if img.size > 0:
+                    cv2.imshow('image', img)
+                    cv2.waitKey(1)
+            except amcam.HRESULTException as ex:
+                print('pull image failed, hr=0x{:x}'.format(ex.hr))
+        else:
+            print('event callback: {}'.format(nEvent))
+        
+
+    def run(self):
+        a = amcam.Amcam.EnumV2()
+        if len(a) > 0:
+            print('{}: flag = {:#x}, preview = {}, still = {}'.format(a[0].displayname, a[0].model.flag, a[0].model.preview, a[0].model.still))
+            for r in a[0].model.res:
+                print('\t = [{} x {}]'.format(r.width, r.height))
+            self.hcam = amcam.Amcam.Open(a[0].id)
+            if self.hcam:
+                try:
+                    width, height = self.hcam.get_Size()
+                    self.width = width
+                    self.height = height
+                    bufsize = ((width * 24 + 31) // 32 * 4) * height
+                    print('image size: {} x {}, bufsize = {}'.format(width, height, bufsize))
+                    self.buf = bytes(bufsize)
+                    if self.buf:
+                        try:
+                            self.hcam.StartPullModeWithCallback(self.cameraCallback, self)
+                        except amcam.HRESULTException as ex:
+                            print('failed to start camera, hr=0x{:x}'.format(ex.hr))
+                    input('press ENTER to exit')
+                finally:
+                    self.hcam.Close()
+                    self.hcam = None
+                    self.buf = None
+            else:
+                print('failed to open camera')
+        else:
+            print('no camera found')
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
